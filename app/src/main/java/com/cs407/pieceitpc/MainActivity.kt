087dbadaff40f570041b1731172a89c814c289dc
Emili Robles <ejrobles@wisc.edu>
2024-10-28 13:23:08 -0500

    added menu and starter buttons

--- app/src/main/java/com/cs407/pieceitpc/MainActivity.kt
+++ app/src/main/java/com/cs407/pieceitpc/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.cs407.pieceitpc
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.profileOption -> {
+                Toast.makeText(this, "Profile Option Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.settingsOption -> {
+                Toast.makeText(this, "Setting Option Selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
