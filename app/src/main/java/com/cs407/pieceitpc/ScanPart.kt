793001e6a3311ef43efcc270d2cca9e99f800b80
Emili Robles <ejrobles@wisc.edu>
2024-11-17 18:35:29 -0600

    card slider

--- app/src/main/java/com/cs407/pieceitpc/ScanPart.kt
+++ app/src/main/java/com/cs407/pieceitpc/ScanPart.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
-import androidx.navigation.fragment.findNavController
 
 
-class PCBuildHighlights : Fragment() {
+class ScanPart : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +18,15 @@ class PCBuildHighlights : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //show back button
-        setupBackNavigation()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scan_parts, container, false)
+
     }
+
 
     //might not be right but starting here
     //findNavController().popBackStack()
-    private fun setupBackNavigation() {
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                findNavController().popBackStack()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
-    }
+
 
 
 }
