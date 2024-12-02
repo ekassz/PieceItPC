128e1e5418a0792523034dba0074c3e828c6c481
Emili Robles <ejrobles@wisc.edu>
2024-12-01 21:28:15 -0600

    made newPCBuild Page

--- app/src/main/java/com/cs407/pieceitpc/PCNewBuild.kt
+++ app/src/main/java/com/cs407/pieceitpc/PCNewBuild.kt
@@ -1,10 +1,15 @@
 package com.cs407.pieceitpc
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
@@ -12,6 +17,14 @@ import com.google.android.material.appbar.MaterialToolbar
 
 
 class PCNewBuild : Fragment() {
+    private lateinit var pcImageView: ImageView
+    private lateinit var uploadImageButton: Button
+    private lateinit var buildNameEditText: EditText
+    private lateinit var buildDescriptionEditText: EditText
+    private lateinit var partsInputEditText: EditText
+    private lateinit var submitButton: Button
+    private var imageUri: Uri? = null
+    private val PICK_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +45,29 @@ class PCNewBuild : Fragment() {
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         // Set up the back button functionality
         toolbar.setNavigationOnClickListener {
-            findNavController().navigateUp() // Navigate back to the previous fragment
+            // Navigate back to the previous fragment
+            findNavController().navigateUp()
+        }
+
+        uploadImageButton = view.findViewById(R.id.uploadImageButton)
+        pcImageView = view.findViewById(R.id.pcImageUpload)
+
+        // Uploading PC Image Selection
+        uploadImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == AppCompatActivity.RESULT_OK) {
+            imageUri = data?.data
+            pcImageView.setImageURI(imageUri) // Display selected image
+        }
+    }
 
-    //might not be right but starting here
-    //findNavController().popBackStack()
     private fun setupBackNavigation() {
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
