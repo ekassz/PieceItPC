bcca91fbba758dabba59d3d80ad62f93a4007b24
anishaapte <156028381+anishaapte@users.noreply.github.com>
2024-12-05 20:56:32 -0600

    Update CardAdapter.kt

--- app/src/main/java/com/cs407/pieceitpc/CardAdapter.kt
+++ app/src/main/java/com/cs407/pieceitpc/CardAdapter.kt
@@ -1,29 +1,20 @@
 package com.cs407.pieceitpc
 
-
-import android.util.Log
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-
 
 
 class CardAdapter(private val buildList: List<CardItem>, homeScreen : HomeScreenFragment, viewModel: UserViewModel) :
     RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
         val parent : HomeScreenFragment = homeScreen
-
-class CardAdapter(
-    private val buildList: List<CardItem>,
-    private val homeScreen : Fragment,
-    viewModel: UserViewModel) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
-        val parent = homeScreen
-
         val viewModel = viewModel
 
 
@@ -42,29 +33,11 @@ class CardAdapter(
 
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val currentBuild = buildList[position]
-
         holder.buildImage.setImageResource(currentBuild.imageResId.toInt())
-
-
-
-
         holder.buildTitle.text = currentBuild.title
         holder.buildDescription.text = currentBuild.description
         holder.buildAuthor.text = "by ${currentBuild.author}"
 
-
-
-
-        Log.d("CardAdapter", "Loading image for: ${currentBuild.title}, Path: ${currentBuild.imageResId}")
-
-        //Load the Image
-        Glide.with(homeScreen)
-            .load(currentBuild.imageResId)
-            .placeholder(R.drawable.pcdefault)
-            .error(R.drawable.pcdefault)
-            .into(holder.buildImage)
-
-        // Set click listener for the card
 
         holder.itemView.setOnClickListener {
             viewModel.setBuildVal(currentBuild.id)
